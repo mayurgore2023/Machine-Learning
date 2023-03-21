8c413d7a33070d6b559a39aaa6b720b7eccdc4c0
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-03-21 13:09:35 +0530

    using KNN

--- Iris classification 1.py
+++ Iris classification 1.py
@@ -1,22 +1,32 @@
+# import libraries:
+
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-def MarvellousKNeighborsClassifier():
-    Dataset = load_iris()       # 1 Load the data
 
+#  Load the data
+def MarvellousKNeighborsClassifier():
+    Dataset = load_iris()       
+
+ #  separate out dependent and independent functions:
     Data = Dataset.data
     Target = Dataset.target
 
+ # splitting of dataset:
     Data_train, Data_test, Target_train, Target_test = train_test_split(Data, Target, test_size = 0.5)
 
+# choose classifier:
     Classifier = KNeighborsClassifier()
 
+# training of dataset:
     Classifier.fit(Data_train, Target_train)
 
+# testing of data:
     Predictions = Classifier.predict(Data_test)
 
+# finding out accuracy of model:
     Accuracy = accuracy_score(Target_test, Predictions)
 
     return Accuracy
