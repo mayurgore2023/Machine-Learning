51b995d23cd5d095ceae63f798febfdf82dcedb8
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-03-21 14:18:41 +0530

    Add files via upload

---
+++ Data Processing = Label Encoding.py
@@ -0,0 +1,57 @@
+# Import Libraries
+import pandas as pd
+import numpy as np
+
+#('Import Data :
+
+print('Import Data :')
+Dataset = pd.read_csv("Data.csv")
+print(Dataset)
+print("_---------------------------------------------------------------------_")
+
+Unique = Dataset['Name'].unique()
+print(Unique)
+print("_---------------------------------------------------------------------_")
+
+
+Count = Dataset['Insurance'].value_counts()
+print(Count)
+print("_---------------------------------------------------------------------_")
+
+from sklearn.preprocessing import LabelEncoder
+
+Encoder = LabelEncoder()
+
+# Encoding of Independant_Variable
+Dataset['Name'] = Encoder.fit_transform(Dataset['Name'])
+
+# Encoding of Dependant_Variable
+Dataset['Insurance'] = Encoder.fit_transform(Dataset['Insurance'])
+
+
+New_Dataset  = Dataset
+
+#separating of Dependant And independant Variables
+
+X = New_Dataset[['Name','Age','Salary']].values
+print(X)
+
+Y = New_Dataset['Insurance'].values
+print(Y)
+
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+Classifier = RandomForestClassifier()
+
+train = Classifier.fit(X,Y)
+
+Prediction = Classifier.predict([[6,42,36000]])
+print(Prediction)
+
+
+
+
+
+
---
+++ Data Processing = Onehot Encoding.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+
+
+# loading of csv file:
+Data = pd.read_csv('OnehotEncoding.csv')
+print(Data)
+print('--------------------------------------------------------------')
+
+print(Data.dtypes)
+print('--------------------------------------------------------------')
+
+Country_Name = Data['Country'].unique()
+print(Country_Name)
+print()
+
+Country_Count = Data['Country'].value_counts()
+print(Country_Count)
+print('--------------------------------------------------------------')
+
+
+Colour_Name = Data['Colour'].unique()
+print(Colour_Name)
+print()
+
+Colour_Count = Data['Colour'].value_counts()
+print(Colour_Count)
+
+print('--------------------------------------------------------------')
+
+
+# Using Onehot encoder:
+
+Encoder = OneHotEncoder()
+
+print('making araay of fetures with encoding')
+Features_array = Encoder.fit_transform(Data[['Country','Colour']]).toarray() # .toarray() make array
+print(Features_array)
+print()
+
+Feature_categories =  Encoder.categories_
+print(Feature_categories)
+
+print('--------------------------------------------------------------')
+# categories saved in differant are we save it in singal array by using numpy
+
+Feature_labels = np.array(Feature_categories).ravel() # .ravel() makes single array
+print(Feature_labels)
+print('---------------------------------------------------------------')
+
+# makinng of Datafram of all encoded features
+
+Dataframe = pd.DataFrame(Features_array,columns= Feature_labels)
+print(Dataframe)
+print("----------------------------------------------------------------")
+
+# combine the Dataframe with original Data :
+
+Datframe_And_Data = pd.concat([Data,Dataframe],axis=1)  # .concat = Join to data Frames \ axis = to concat along
+print(Datframe_And_Data)
+
+
+
+
+
