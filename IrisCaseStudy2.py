7f6bf98f02e632718045898c5348cfc5754998fb
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-03-21 13:14:22 +0530

    iris classification using DTC

--- IrisCaseStudy2.py
+++ IrisCaseStudy2.py
@@ -1,23 +1,34 @@
+# iris classification using Decission tree clasifier:
+
+
+# import libraries:
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 def MarvellousDecisionTreeClassifier():
-    Dataset = load_iris()       # 1 Load the data
+     # 1 Load the data
+    Dataset = load_iris()     
 
+     # separatout depenant and independant variables:
     Data = Dataset.data
     print(Data)
     Target = Dataset.target
-
+    
+    # splitting of dataset:
     Data_train, Data_test, Target_train, Target_test = train_test_split(Data, Target, test_size = 0.5)
-
+    
+    # classifier:
     Classifier = DecisionTreeClassifier()
-
+    
+    # training:
     Classifier.fit(Data_train, Target_train)
-
+    
+    # testing:
     Predictions = Classifier.predict(Data_test)
-
+    
+    # accuracy:
     Accuracy = accuracy_score(Target_test, Predictions)
 
     return Accuracy
