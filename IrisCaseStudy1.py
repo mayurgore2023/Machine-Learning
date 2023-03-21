eccba129b764bc494cb99a4bddc75dd30566e495
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-03-21 13:02:59 +0530

    c

---
+++ IrisCaseStudy1.py
@@ -0,0 +1,30 @@
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+def MarvellousKNeighborsClassifier():
+    Dataset = load_iris()       # 1 Load the data
+
+    Data = Dataset.data
+    Target = Dataset.target
+
+    Data_train, Data_test, Target_train, Target_test = train_test_split(Data, Target, test_size = 0.5)
+
+    Classifier = KNeighborsClassifier()
+
+    Classifier.fit(Data_train, Target_train)
+
+    Predictions = Classifier.predict(Data_test)
+
+    Accuracy = accuracy_score(Target_test, Predictions)
+
+    return Accuracy
+
+def main():
+    Ret = MarvellousKNeighborsClassifier()
+
+    print("Acuracy of Iris dataset with KNN is ",Ret * 100)
+
+if __name__ == "__main__":
+    main()
